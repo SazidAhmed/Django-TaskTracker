c0cb64ded06818627d2530fd418f8dcdc3337fc6
SazidAhmed <sazidahmed.official@gmail.com>
2021-04-04 00:16:35 +0600

    teammate time tracker bug fixed

--- apps/adminpanel/views.py
+++ apps/adminpanel/views.py
@@ -1,6 +1,7 @@
 # Import Python
 from datetime import datetime, timedelta, timezone, date
 from dateutil.relativedelta import relativedelta
+import pytz
 
 # Import Django
 from django.contrib.auth.decorators import login_required
@@ -28,19 +29,19 @@ def dashboard(request):
 
     # User date, pagination
     num_days = int(request.GET.get('num_days', 0))
-    date_user = datetime.now() - timedelta(days=num_days)
+    date_user = datetime.now(timezone.utc) - timedelta(days=num_days)
     date_entries = Entry.objects.filter(team=team, created_by=request.user, created_at__date=date_user, is_tracked=True)
     
     # User month, pagination
     user_num_months = int(request.GET.get('user_num_months', 0))
-    user_month = datetime.now()-relativedelta(months=user_num_months)
+    user_month = datetime.now(timezone.utc)-relativedelta(months=user_num_months)
 
     for project in all_projects:
         project.time_for_user_and_project_and_month = get_time_for_user_and_project_and_month(team, project, request.user, user_month)
     
     # Team month, pagination
     team_num_months  = int(request.GET.get('team_num_months', 0))
-    team_month = datetime.now()-relativedelta(months=team_num_months)
+    team_month = datetime.now(timezone.utc)-relativedelta(months=team_num_months)
 
     for member in members:
         member.time_for_user_and_team_and_month = get_time_for_user_and_team_month(team, member, team_month)
@@ -81,34 +82,35 @@ def view_user(request, user_id):
     team = get_object_or_404(Team, pk=request.user.userprofile.active_team_id, status=Team.ACTIVE)
     all_projects = team.projects.all()
     user = team.members.all().get(id=user_id)
-
+    # print(user.id)
     # User date, pagination
 
     num_days = int(request.GET.get('num_days', 0))
-    date_user = datetime.now() - timedelta(days=num_days)
+    date_user = datetime.now(timezone.utc) - timedelta(days=num_days)
 
-    date_entries = Entry.objects.filter(team=team, created_by=request.user, created_at__date=date_user, is_tracked=True)
+    date_entries = Entry.objects.filter(team=team, created_by=user.id, created_at__date=date_user, is_tracked=True)
     
     # User month, pagination
 
     user_num_months = int(request.GET.get('user_num_months', 0))
-    user_month = datetime.now()-relativedelta(months=user_num_months)
+    user_month = datetime.now(timezone.utc)-relativedelta(months=user_num_months)
 
     for project in all_projects:
-        project.time_for_user_and_project_and_month = get_time_for_user_and_project_and_month(team, project, request.user, user_month)
+        project.time_for_user_and_project_and_month = get_time_for_user_and_project_and_month(team, project, user, user_month)
 
     # Context
 
     context = {
         'team': team,
+        'user': user,
         'all_projects': all_projects,
         'date_entries': date_entries,
         'num_days': num_days,
         'date_user': date_user,
         'user_num_months': user_num_months,
         'user_month': user_month,
-        'time_for_user_and_month': get_time_for_user_and_month(team, request.user, user_month),
-        'time_for_user_and_date': get_time_for_user_and_date(team, request.user, date_user),
+        'time_for_user_and_month': get_time_for_user_and_month(team, user, user_month),
+        'time_for_user_and_date': get_time_for_user_and_date(team, user, date_user),
     }
 
     return render(request, 'view_user.html', context)
--- ttm/settings.py
+++ ttm/settings.py
@@ -120,6 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
+# TIME_ZONE = 'asia/dhaka'
 
 USE_I18N = True
 
@@ -155,8 +156,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'oceanta.fashion@gmail.com' 
-EMAIL_HOST_PASSWORD = '123456'
+EMAIL_HOST_USER = 'sazidahmed.official@gmail.com' 
+EMAIL_HOST_PASSWORD = 'mregmwistizxkqut'
 DEFAULT_EMAIL_FROM = 'oceanta.fashion@gmail.com'
 
 WEBSITE_URL = 'http://127.0.0.1:8000'
