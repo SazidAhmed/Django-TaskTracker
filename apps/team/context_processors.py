0d71af7c421ed9f55525cde41e99ba662bc76f1c
SazidAhmed <sazidahmed.official@gmail.com>
2021-06-02 17:43:19 +0600

    context updated

--- apps/team/context_processors.py
+++ apps/team/context_processors.py
@@ -3,9 +3,11 @@ from .models import Team
 
 def active_team(request):
     if request.user.is_authenticated:
-        if request.user.userprofile.active_team_id:
-            team = Team.objects.get(pk=request.user.userprofile.active_team_id)
+        if request.user.userprofile:
+            if request.user.userprofile.active_team_id:
+                team = Team.objects.get(pk=request.user.userprofile.active_team_id)
 
-            return {'active_team': team}
-            
+                return {'active_team': team}
+            return {'active_team': None}
+        return {'active_team': None}
     return {'active_team': None}
